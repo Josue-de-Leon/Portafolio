c3028e261e6a5f10c9dd48fb847f501b58015618
Josue-de-Leon <jmayend1@miumg.edu.gt>
2024-07-20 02:55:18 -0600

    Se restauro el Nav, footer y algunos textos

--- js/main.js
+++ js/main.js
@@ -74,13 +74,52 @@ function certificar() {
     $project.style.border="none";
     $certi.style.borderBottom="2px solid orange";
 }
-function corazon(){
-    document.getElementById('corazon1').style.display = 'none';
-    document.getElementById('corazon2').style.display = 'block';
+function corazon(dato){
+    console.log("Dato recibido:", dato);
+    switch(dato){
+        case 1:
+            document.getElementById('corazon_abierto').style.display = 'none';
+            document.getElementById('corazon_cerrado').style.display = 'block';
+        break;
+        case 2:
+            document.getElementById('corazon_abierto2').style.display = 'none';
+            document.getElementById('corazon_cerrado2').style.display = 'block';
+        break;
+        case 3:
+            document.getElementById('corazon_abierto3').style.display = 'none';
+            document.getElementById('corazon_cerrado3').style.display = 'block';
+        break;
+    }
+    
 }
-function corazon2(){
-    document.getElementById('corazon1').style.display = 'block';
-    document.getElementById('corazon2').style.display = 'none';
+function corazon2(dato2){
+    console.log("Dato recibido corazon:", dato2);
+    switch (dato2){
+        case 1:
+            document.getElementById('corazon_abierto').style.display = 'block';
+            document.getElementById('corazon_cerrado').style.display = 'none';
+        break;
+        case 2:
+            document.getElementById('corazon_abierto2').style.display = 'block';
+            document.getElementById('corazon_cerrado2').style.display = 'none';
+        break;
+        case 3:
+            document.getElementById('corazon_abierto3').style.display = 'block';
+            document.getElementById('corazon_cerrado3').style.display = 'none';
+        break;
+    }
+    
+}
+function ver(dato3){
+    switch(dato3){
+        case 1:
+            window.open("https://josue2023.neocities.org/netflix/", "_blank");
+        break; 
+        case 2:
+            window.open("https://guatemaladigital.com/", "_blank");
+        break; 
+    }
+    
 }
 
 
@@ -89,15 +128,25 @@ var imgContainer = document.getElementById("imgContainer");
   // Función para cambiar la imagen cuando el tamaño de la ventana cambia
   function cambiarImagen() {
     var img = imgContainer.querySelector("img");
+    var imgContainer2 = document.getElementById("imgContainer2");
+    var img2 = imgContainer2.querySelector("img");
 
     // Obtener el ancho del contenedor
     var containerWidth = imgContainer.offsetWidth;
+    var containerWidth2 = imgContainer2.offsetWidth;
 
     // URL de la imagen alternativa
     var nuevaImagenURL = (containerWidth <= 600) ? "../img/code 2.jpg" : "img/code.jpg";
+    var nuevaImagenURL2 = (containerWidth2 <= 767) ? "../img/GD Logo 3.png" : "img/GD Logo 2.png";
+    var nuevaImagenURL4 = (containerWidth2 <= 991) ? "../img/GD Logo 5.png" : "img/GD Logo 2.png";
+    var nuevaImagenURL3 = (containerWidth2 <= 1199) ? "../img/GD Logo 4.png" : "img/GD Logo 2.png";
 
     // Cambiar la fuente de la imagen
     img.src = nuevaImagenURL;
+    img2.src = nuevaImagenURL2;
+    img2.src = nuevaImagenURL2;
+    img2.src = nuevaImagenURL3;
+    img2.src = nuevaImagenURL4;
   }
    // Ejecutar la función cuando se carga la página y cuando cambia el tamaño de la ventana
    window.addEventListener("load", cambiarImagen);
